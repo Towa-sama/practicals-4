ee5d7f285449b8a5f68b4835f25d3cd51dc38ac9
Towa-sama <74224592+Towa-sama@users.noreply.github.com>
2020-12-09 12:01:05 +0600

    Add files via upload

---
+++ total_income.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 Practical
+Total income program
+"""
+
+
+def main():
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(
+            input("Enter income for month {}: ".format(month)))
+        incomes.append(income)
+
+    print_report(incomes)
+
+
+def print_report(incomes):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month, income in enumerate(incomes):
+        total += income
+        print("Month {:2} - Income: ${:6.2f} \
+        Total: ${:6.2f}".format(month + 1, income, total))
+
+
+main()
